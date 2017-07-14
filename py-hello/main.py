7b8a4cf78600c204eca74645f2c0db6737adc324
ajaytr <ajaytr@hotmail.com>
2017-07-14 16:27:23 -0400

    Added main file and initial server test

---
+++ py-hello/main.py
@@ -0,0 +1,31 @@
+import datetime
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# HTTPRequestHandler class
+class basicHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Python says: Hello World!\n"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+
+if __name__ == "__main__":
+    now = datetime.datetime.now()
+    print ("Starting server at time: " + str(now) + "\n")
+
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = ('127.0.0.1', 8081)
+    httpd = HTTPServer(server_address, basicHTTPServer_RequestHandler)
+    httpd.serve_forever()
